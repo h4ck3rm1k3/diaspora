9f1369b72f03319e3e0e23bd63498d694eb1534c
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2011-10-17 13:45:06 +0200

    cleaning

--- tests/testar.rb
+++
@@ -1,4 +0,0 @@
-require 'rubygems'
-require 'active_record'
-
-#ActiveRecord::Base.establish_connection( :adapter  => 'mysql',  :database => 'database',  :username => 'user',  :password => 'password',  :host     => 'localhost')
